b6252859ed2bb2a6165d8115dc008a22f9fea3d3
Rudaina Binte Tanveer <rudainarbt@gmail.com>
2024-08-04 17:59:20 +0500

    JavaScript Practice

---
+++ app.js
@@ -0,0 +1,418 @@
+// // Alerts
+// // Question# 01
+// // i
+// // alert("Rudaina")
+// // ii
+// // alert("Tanveer")
+// // iii
+// // alert("rudainarbt@gmail.com")
+// // iv
+// // alert("03332262281")
+// // v
+// // alert("********")
+// // Question# 02
+// // alert("You're Learning JavaScript!")
+// // Question# 03
+// // alert("Hi there!")
+
+// // Variables for Strings
+// // Question# 01
+// // let underGrad = "Software Engineering"
+// // Question# 02
+// // let batch
+// // batch = "2023"
+// // Question# 03
+// // let teamName = "Coding Freaks"
+// // alert(teamName)
+// // Question# 04
+// // var bestMan = "Charlie";
+// // var newVar = bestMan;
+
+// // Variables for Numbers
+// // Question# 01
+// // let caseQty;
+// // Question# 02
+// // let variable = 144
+// // caseQty = variable;
+// // Question# 03
+// // var num = "9"
+// // Question# 04
+// // let sum;
+// // sum = 10 + 20;
+// // Question# 05
+// // let merchTotal = 100;
+// // let shippingCharge = 10;
+// // let total = merchTotal + shippingCharge; //110
+// // Question# 06
+// // let a = 10
+// // a += 12
+
+// // Variable names Legal and Illegal
+// // Question# 01
+// // var productCost = 3.45;
+// // Question# 02
+// // var nameOfBand;
+// // Question# 03
+// // var myAge = 18
+// // Question# 04
+// // var rudainaTanveer
+// // Question# 05
+// // All the already defined keywords are illegal to use as variable name 
+// // Those names which consist of spaces or some special characters are also illeagal 
+// // Names based only on the numbers are also illegal
+// // Rest are legal
+
+// // Math Expression I
+// // Question# 01
+// // Modulus %
+// //     Question# 02
+// // 2
+// // Question# 03
+// // var largeNum = 1000 * 2000
+// // Question# 04
+// // var a = 3
+// // var b = 5
+// // var c = b - a
+// // Question# 05
+// // var a = 20
+// // var div = a % 5
+// // Question# 06
+// // alert(33 * 5)
+
+// // Math Expression II
+// // Question# 01
+// // x += 1
+// // Question# 02
+// // --x
+// // Question# 03
+// // y = 50(Since first it prints then it increments)
+// // Question# 04
+// // z = 49
+// // Question# 05
+// // var newNum = num--
+// // Question# 06
+// // var y = x++
+// // Question# 07
+// // var num = 90
+// // ++num
+// // alert(num)
+
+// // Math Expression III
+// // Question# 01
+// // calculatedNum = 14
+// // Question# 02
+// // calculatedNum = 24
+// // Question# 03
+// // calculatedNum = 24
+// // Question# 04
+// // calculatedNum = 18
+// // Question# 05
+// // var newNum = (2 + 2) * (4 + 10) // = 56
+// // Question# 06
+// // var newNum = 2 + (2 * 4) + 10 // = 20
+// // Question# 07
+// // var newNum = 4 / (2 * 4)
+
+// // Concatenating Text Strings
+// // Question# 01
+// // num = "22"
+// // Question# 02
+// // message = "Hello Dolly"
+// // alert(message)
+// // Question# 03
+// // 333
+// // Question# 04
+// // alert("Pakistan" + "Zindabad")
+// // Question# 05
+// // var newString = ("My age is " + 18)
+// // Question# 06
+// // var a = "Jawan"
+// // var b = "Pakistan"
+// // var c = a + b
+
+// // Prompts
+// // Question# 01
+// // var firstName = prompt("Enter first name")
+// // Question# 02
+// // var country = prompt("Country?", "China")
+// // alert(message)
+// // Question# 03
+// // var yourName = prompt("Enter Your Name");
+// // Question# 04
+// // var yourName = prompt("Greet", "Hellow World")
+// // Question# 05
+// // var msg = "My age is"
+// // var output = "18"
+// // prompt(msg, output)
+// // Question# 06
+// // var yourName = prompt("Enter Your Name", "RBT");
+// // alert("Hello, " + yourName + "!");
+
+// // If statments
+// // Question# 01
+// // var city = "Karachi"
+// // if (city == "Karachi") {
+// //     console.log("The City OF Lights")
+// // }
+// // Question# 02
+// // if (x === y) {
+// //     z = prompt("Enter the value for z")
+// //     var newVariable = z
+// // }
+// // Question# 03
+// // var zipCode
+// // if (zipCode === "100010") {
+// //     alert("Karachi")
+// // }
+// // else {
+// //     alert("Please write correct city")
+// // }
+// // }
+// // Question# 04
+// // if (x == 1) {
+// //     x = 4
+// // }
+
+// // Comparison Operators
+// // Question# 01
+// // if (x != 1)
+// // Question# 02
+// // if (x > y)
+// // Question# 03
+// // if (x != 1) {
+// //     x = 55
+// // }
+// // Question# 04
+// // if (x == "10") {
+// //     alert("Congratulations")
+// // }
+// // Question# 05
+// // var name = prompt("Enter your name")
+// // if (name != "RBT") {
+// //     alert("No match")
+// // }
+
+// // (if...else and else if statements)
+// // Question# 01
+// // if (x == "10") {
+// //     alert("Matched")
+// // }
+// // else {
+// //     alert("Not matched")
+// // }
+// // Question# 02
+// // var eng = +prompt("Enter your marks in English") % 100;
+// // var isl = +prompt("Enter your marks in Islamiat") % 100;
+// // var math = +prompt("Enter your marks in Math") % 100;
+// // var urdu = +prompt("Enter your marks in Urdu") % 100;
+// // var total = eng + isl + math + urdu;
+// // var percentage = (total / 400) * 100;
+// // document.write("MARKS = " + eng + "<br>");
+// // document.write("MARKS = " + math + "<br>");
+// // document.write("MARKS = " + urdu + "<br>");
+// // document.write("MARKS = " + isl + "<br>");
+
+// // if (percentage >= 80 && percentage <= 100) {
+// //     document.write("Your grade is A+");
+// // }
+// // else if (percentage >= 70 && percentage <= 79) {
+// //     document.write("Your grade is A");
+// // }
+// // else if (percentage >= 60 && percentage <= 69) {
+// //     document.write("Your grade is B");
+// // }
+// // else if (percentage >= 50 && percentage <= 59) {
+// //     document.write("Your grade is C");
+// // }
+// // else {
+// //     document.write("Your grade is F");
+// // }
+// // Question# 03
+// // if (a === 10) {
+// //     alert("a is 10");
+// // }
+// // else {
+// //     alert("the value of a is" + a)
+// // }
+// // Question# 04
+// // var city = prompt("Enter a city")
+// // if (city == "Karachi") {
+// //     alert("The city is Karachi")
+// // }
+// // else if (city == "Lahore") {
+// //     alert("The city is Lahore")
+// // }
+// // else {
+// //     alert("The city is " + city)
+// // }
+
+// // Testing sets of conditions
+// // Question # 01
+// // if (a == b && c == d)
+// // Question # 02
+// // if (a == b || c == d)
+// // Question # 03
+// // if (name == "Hamza" && name == "Arsalan")
+// // Question # 04
+// // var a = 10
+// // var b = 20
+// // if (a < b || a > b) {
+// //     alert("the values are unequal")
+// // }
+// // Question # 05
+// // var first = "Rudaina"
+// // var last = "Tanveer"
+// // var fname = prompt("Enter first name")
+// // var lname = prompt("Enter last name")
+// // if (first == fname && last == lname) {
+// //     alert("You entered correct name")
+// // }
+
+// // If statements nested
+// // Question # 01
+// // if (password != "") {
+// //     if (password < 5) {
+// //         alert("Password must be greater than 5")
+// //     }
+// //     else if (password > 5) {
+// //         alert("OK")
+// //     }
+// // }
+// // Question # 02
+// // if (a === 1) {
+// //     if (c === "Max") {
+// //         alert("OK");
+// //     }
+// // }
+// // Question # 03
+// // if (a === 1 || (c === "Max") {
+// //     alert("OK");
+// // }
+// // Question # 04
+// // var a = 10
+// // var b = 10
+// // if (a == b) {
+// //     if (a <= b) {
+// //         alert("the values are equal")
+// //     }
+// // }
+
+// // Array I
+// // Question # 01
+// // var arr = []
+// // Question # 02
+// // arr = ['h']
+// // Question # 03
+// // var alphabet = ["h", "i", "j", "k"]
+// // alert(alphabet[2])
+// // Question # 04
+// // var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"]
+// // var totalLength = alphabet.indexOf("o") + 1
+// // console.log(totalLength)
+// // Question # 05
+// // var anArray = [0]
+// // anArray[1] = 2
+// // alert("New element" + anArray[1])
+
+// // Array II
+// // Question # 01
+// // var arr = ["r"]
+// // arr.push("b")
+// // alert("the last element is" + arr[1])
+// // Question # 02
+// // var Alphabet = ["h", "i", "j", "k"]
+// // Alphabet.pop()
+// // Question # 03
+// // Alphabet.push(1816)
+
+// // Array III
+// // Question # 01
+// // var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+// // sizes.shift()
+// // Question # 02
+// // sizes.unshift(3)
+// // Question # 03
+// // var newArray = [23]
+// // newArray.unshift[21]
+// // alert("New element" + newArray[0])
+// // Question # 04
+// // var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+// // sizes.splice(2, 0, "L")
+// // Question # 05
+// // var regSizes = sizes.splice(0, 3)
+// // Question # 06
+// // var pets = ["dog", "cat", "ox", "duck", "frog"];
+// // pets.splice(1, 3, "parrot", "hamster");
+// // Question # 07
+// // var pets = ["dog", "cat", "ox", "duck", "frog"];
+// // pets.splice(1, 2); 
+// // Question # 08
+// // var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// // var reducedPets = pets.slice(3, 5);
+
+// // For Loops
+// // Question # 01
+// // for (var i = 0; i = 10; i++) {
+// //     document.write("Let's learn JS")
+// // }
+// // Question # 02
+// // for (var i = 0; i <= 12; i++) {
+// //     document.write(i)
+// // }
+// // Question # 03
+// // for (var i = 0; i <= 4; i++) { }
+// // Question # 04
+// // for (var j = 0; j < 100; j++) {
+// //     document.write(i)
+// // }
+// // Question # 05
+// // for (var i = 3; i < 0; i--) {
+// //     document.write(i)
+// // }
+// // Question # 06
+// // var arr = [0, 1, 2, 3]
+// // var num = arr.length()
+// // Question # 07
+// // var flag = true
+// // Question # 08
+// // for (var i = 0; i <= pets.length(); i++)
+// // Question # 09
+// // for (var i = 0; i = 10; i++) {
+// //     document.write(i)
+// //     if (i = 2) {
+// //         alert(1)
+// //         break;
+// //     }
+// // }
+// // Question # 10
+// // var usernames = ["Ali", "Eman", "Hassan", "Alizay", "Asma"]
+// // firstname = prompt("Enter first name")
+// // for (var i = 0; i = username.length(); i++) {
+// //     if (usernames[i] = firstname) {
+// //         alert("Enter")
+// //     }
+// //     else {
+// //         alert("Please write correct username")
+// //     }
+
+// // }
+// // Question # 11
+// // var matchFound = false;
+// for (var i = 0; i < list.length; i++) {
+//     if (userInput === list[i]) {
+//         alert("Match found");
+//         matchFound = true;
+//         break;
+//     }
+//     else {
+//         alert("Match not found");
+//     }
+// };
+// // Question # 12
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+// for (var i = 0; i < firstArr.length(); i++) {
+//     for (var j = 0; j < secondArrtArr.length(); j++) {
+//         document.write(firstArr[i]+secondArr[j])
+//     }
+// }
