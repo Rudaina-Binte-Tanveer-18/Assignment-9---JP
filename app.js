633e819bad1a37716fb86a4aaec710443c0bdc06
Rudaina-Binte-Tanveer-18 <154063207+Rudaina-Binte-Tanveer-18@users.noreply.github.com>
2024-08-21 17:06:46 +0500

    Assignment Done

--- app.js
+++ app.js
@@ -417,3 +417,186 @@
 //     }
 // }
 
+// // Changing Case
+// // Question # 01
+// var allLower = userInput.toLowerCase()
+// // Question # 02
+// var x = "STRING"
+// x = x.toLowerCase()
+// // Question # 03
+// var y = "string"
+// y = y.toUpperCase()
+// // Question # 04
+// var z = "STRING"
+// var lower = z.toLowerCase();
+// // Question # 05
+// let arr = ["ALI", "SARA", "AHMED", "AMNA"]
+// let low = arr[2].toLowerCase()
+// // Question # 06
+// let myname = "rudaina"
+// myname = myname.toUpperCase()
+// alert(myname)
+// // Question # 07
+// var cityName = "kaRacHi"
+// cityName = cityName.charAt(0).toUpperCase() + cityName.charAt(1).toLowerCase() + cityName.charAt(2).toLowerCase() + cityName.charAt(3).toLowerCase() + cityName.charAt(4).toLowerCase() + cityName.charAt(5).toLowerCase() + cityName.charAt(6).toLowerCase()
+// console.log(cityName)
+
+// Strings
+// // Question # 01
+// var sameWords = "captain"
+// var wordSlice = sameWords.slice(1, 3)
+// console.log(wordSlice)
+// // Question # 02
+// var strLen = sameWords.length()
+// // Question # 03
+// var animal = "elephant"
+// var seg = animal.slice(2, 6)
+// console.log(seg)
+// // Question # 04
+// var sameWords = "captain"
+// var strLen = sameWords.length()
+// // Question # 05
+// let str = "GoodMorning";
+// let lengthString = str.length;
+// let newStr = str.slice(1, -3);
+// Question # 06
+// var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+// console.log(indx) = 3
+// Question # 07
+// var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// console.log(indx) 
+// Question # 08
+// var text = "I can go to Turkeyie but my Parents won't allow me to go alone"
+// var indx = text.lastIndexOf("go");
+// Question # 09
+// var indexNum = 3
+// var string = "Rudaina"
+// if(string.charAt(indexNum) != null){
+//     console.log(string)
+// }
+// else{
+//     console.log("Your string is not fit")
+// }
+// Question # 10
+// var str = "abcde"
+// console.log(str.charAt(2))
+// Question # 11
+// var text = "To be or not to be.";
+// var cha = text.charAt(10)
+// Question # 12
+// var str = "Rudaina"
+// var x = str.charAt(str.length)
+// Question # 13
+// var text = "To be or not to be.";
+// var cha = text.charAt(5)
+// Question # 14
+// var indexNum = 3
+// var string = "Rudaina"
+// if(string.charAt(indexNum) != "a"){
+//     console.log(string)
+// }
+// else{
+//     console.log("Your string is not fit")
+// }
+// Question # 15
+// var str = "Rudaina"
+// var arr = []
+// for(let i=0; i<str.length; i++){
+//     arr[i] = str.charAt(i)
+// }
+// console.log(arr)
+
+// var reply = "I didn't want to go to school so i said no to my parents for trip."
+// var revisedReply = reply.replace("no", "yes")
+// console.log(revisedReply)
+// Question # 16
+// var str = "I have 1 pen"
+// var newStr = str.repeat("1", "one")
+// Question # 17
+// var y = x.replaceAll("a", "z")
+
+// Rounding Numbers
+// Question # 01
+// var num = 3.4
+// num = Math.round(num)
+// Question # 02
+// var origNum = 3.42
+// var roundNum = Math.ceil(origNum)
+// Question # 03
+// var origNum = 3.42
+// var roundNum = Math.floor(origNum)
+// Question # 04
+// var num = 3.4
+// var newNum = Math.round(num)
+// Question # 05
+// var origNum = 0.5
+// var roundNum = Math.floor(origNum)
+
+// Random Numbers
+// Question # 01
+// var number = (Math.random()*50)+1
+// console.log(number)
+// Question # 02
+// var number = Math.random()
+// Question # 03
+// var dice = Math.round(Math.random() * 5)+1
+// console.log(dice)
+// Question # 04
+// var toss = Math.round(Math.random() * 1)+1
+// console.log(toss)
+// if(toss == 1){
+//     console.log("Head")
+// }
+// else if(toss == 2) {
+//     console.log("Tails")
+// }
+
+// Converting Strings
+// Question # 01
+// Through Number() or parseInt()
+// Question # 02
+// function str_int(str) {
+//     return parseInt(str, 10);
+// }
+// let str = "123";
+// let integer = str_int(str);
+// console.log(integer);
+// Question # 03
+// let str = "123.45";
+// let floatNum = Number(str);
+// console.log(floatNum);
+// Question # 04
+// function canConvertToInt(str) {
+//     return !isNaN(parseInt(str, 10)) && !str.includes(".");
+// }
+// function canConvertToFloat(str) {
+//     return !isNaN(parseFloat(str));
+// }
+// Question # 05
+// let num = 42;
+// let str = num.toString();
+// console.log(str);
+// Question # 06
+// let num = 42;
+// let str = num.toString();
+// console.log(str)
+// Question # 07
+// let str = "3.14";
+// let intNum = parseInt(str, 10);
+// console.log(intNum)
+
+// Controlling the length of decimals
+// Question # 01
+// var num=4.526971
+// let newNum = num.toFixed(4).toString();
+// Question # 02
+// num = Number(num.toFixed(2).toString());
+// Question # 03
+// if (num.toFixed(2).toString().length > 4) {
+//     console.log(num)
+// }
+// Question # 04
+// let longNum = 123.456789;
+// alert(longNum.toFixed(2).toString());
